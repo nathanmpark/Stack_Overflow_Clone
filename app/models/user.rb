172f44a49e4aa937ac164f84aea002cbca252f3e
Jupiter <email": null>
2015-11-18 15:42:42 -0800

    Create User model and migration, add user controller

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+  include BCrypt
+  has_many :answers
+  has_many :questions
+  has_many :responses
+  validates :email, uniqueness: true
+
+  def password
+    @password = Password.new(password_hash)
+    self.password_hash = @password
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+end
