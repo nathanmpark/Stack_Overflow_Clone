69aab7e9260e1a007f22874e33ef8fa235999143
Kim Allen <kimmarieallen@gmail.com>
2015-11-18 16:56:49 -0800

    Start ajax for answers and controller for answers- in process

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+
+    $('#answer').on('submit'), function(event){
+      event.preventDefault();
+      data = $(this).serialize()
+      $.ajax({method: 'post',
+              url: '/answers',
+              dataType: 'json'})
+      .done(function(response){
+        // put in html <li>response.content</li>
+        // append to answer list
+        $('#answer_list').append('<li>'+ response.content + <span> + response.user_id.username + </span></li>)
+        });
+      });
+    });
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
